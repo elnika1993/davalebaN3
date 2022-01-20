4f98ae1a2fd10e3c9ed333523692f81b12e99ad2
elnika1993 <shubladze.nikoloz@yahoo.com>
2022-01-20 23:44:03 +0400

    davaleba N3

---
+++ davalebaN3.py
@@ -0,0 +1,11 @@
+a = [1, 2, 10, 24, 32, 88]
+b = [1, 2, 32, 48, 72, 88, 100]
+c = []
+print(a)
+print(b)
+while True:
+    def sia(c):
+        c = list(set(a) ^ set(b))
+        print("unikaluri cifrebia=", c)
+    break
+sia(c)
